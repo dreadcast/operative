64d0719203fb1c81e12106a2b48f7b80874d9c60
Raphaël Gully <concept@dreadcast.com>
2015-03-07 11:42:14 +0100

    release task

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = function(gulp){
+// 	require('./asset')(gulp);
+// 	require('./js')(gulp);
+// 	require('./less')(gulp);
+	require('./release')(gulp);
+// 	require('./swig')(gulp);
+};
